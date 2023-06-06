e0937533923c6b9674bbb28688743fc321f71b19
PedroDaCA <pedrocal2002@gmail.com>
2023-06-05 22:14:39 -0600

    Adding a new controller and endpoint

---
+++ BusinessLayer/ServiceInterfaces/IWorkerService.cs
@@ -0,0 +1,22 @@
+﻿using ProductionBusinessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductionBusinessLayer.Interfaces
+{
+    /// <summary>
+    /// Relates to every transaccion that is exclusively for the worker context.
+    /// It could relate to its position, basic information, etc.
+    /// </summary>
+    public interface IWorkerService
+    {
+        /// <summary>
+        /// Gets the basic information of each active worker.
+        /// </summary>
+        /// <returns>A list of the basic information for each active worker.</returns>
+        IList<BasicWorkerInformation> GetBasicActiveWorkerInformation();
+    }
+}
